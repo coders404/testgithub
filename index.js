bda5f4c8dcca79f2e54924118be58e13172c90de
coderbug <75960920+coders404@users.noreply.github.com>
2022-09-28 11:04:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
 console.log('我是github新创建的文件')
 alert('hello')
 var arr = [10,20,30,40]
-console.log(arr)
-function a() {
-  var a = 10
-  console.log(a)
-}
-var a = [10,20,405050505005050]
-console.log(a)
-function Deng() {
-	var aaa = 10020202
-}
+
