d750b543b8c32562b07f7dfa431b5166a0ecc17c
lxl <2846641774@qq.com>
2022-09-28 11:03:37 +0800

    commit index.js 4

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
 console.log('我是github新创建的文件')
-alert('hello')
-var arr = [10,20,30,40]
-console.log(arr)
-function a() {
-  var a = 10
-  console.log(a)
-}
-var a = [10,20,405050505005050]
-console.log(a)
-function Deng() {
-	var aaa = 10020202
-}
+alert('helloworlds')
+alert(3333333)
